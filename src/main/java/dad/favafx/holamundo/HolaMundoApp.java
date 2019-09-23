92c8056b80e3335617cf6dd365e01b2d0f775c7e
Cristian Trujillo <cristiantllotllo2424@gmail.com>
2019-09-23 10:01:58 +0100

    program completed

--- src/main/java/dad/favafx/holamundo/HolaMundoApp.java
+++ src/main/java/dad/favafx/holamundo/HolaMundoApp.java
@@ -1,7 +1,10 @@
 package dad.favafx.holamundo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -16,8 +19,26 @@ public class HolaMundoApp extends Application {
 		saludoLabel.setLayoutX(20);
 		saludoLabel.setLayoutY(20);
 		
+		Button saludarButton = new Button();
+		saludarButton.setText("Saludar");
+		saludarButton.setLayoutX(20);
+		saludarButton.setLayoutY(80);
+		
+		/*saludarButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				saludoLabel.setText("Hola!");
+				saludarButton.setVisible(false);
+			}
+		});*/
+		
+		saludarButton.setOnAction(event -> {
+			saludoLabel.setText("Hola!");
+			saludarButton.setVisible(false);
+		});
+		
 		Pane root = new Pane();
-		root.getChildren().add(saludoLabel);
+		root.getChildren().addAll(saludoLabel, saludarButton);
 		
 		Scene scene = new Scene(root, 320, 200);
 		
